be1da92974392dc4fa6b3f76d4aaf5cdfde98ed3
Yuta <rubystudy0123@gmail.com>
2019-03-05 14:18:46 +0000

    Finish Attendances edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,15 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    if params[:first_day].nil?
-      @first_day = Date.today.beginning_of_month
-    else
-      @first_day = Date.parse(params[:first_day])
-    end
-      @last_day = @first_day.end_of_month
+    @first_day = first_day(params[:first_day])
+    @last_day = @first_day.end_of_month
     (@first_day..@last_day).each do |day|
       unless @user.attendances.any? {|attendance| attendance.worked_on == day }
         record = @user.attendances.build(worked_on: day)
         record.save
       end
     end
-    @dates = @user.attendances.where('worked_on >= ? and worked_on <= ?', @first_day, @last_day).order('worked_on')
+    @dates = user_attendances_month_date
     @worked_sum = @dates.where.not(started_at: nil).count
   end
   
